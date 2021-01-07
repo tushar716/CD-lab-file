852109a2859557f426296aea3e09ce032a49f7a6
tushar716 <77063518+tushar716@users.noreply.github.com>
2021-01-07 12:15:03 +0530

    Update Leading_Trailing.cpp

--- Leading_Trailing.cpp
+++ Leading_Trailing.cpp
@@ -1,185 +1,75 @@
-#include <iostream>
-#include <string.h>
-using namespace std;
-int nt, t, top = 0;
-char s[50], NT[10], T[10], st[50], l[10][10], tr[50][50];
-int searchnt(char a)
+#include<iostream.h>
+#include<conio.h>
+void main()
+{	
+clrscr();
+char s,l[20],r[10],lead[10],trail[10];
+int n,j,m;
+for(int i=0;i<10;i++)
 {
-    int count = -1, i;
-    for (i = 0; i < nt; i++)
-    {
-        if (NT[i] == a)
-            return i;
-    }
-    return count;
+lead[i]=NULL;
+trail[i]=NULL;
 }
-int searchter(char a)
+cout<<"\nenter total no. of productions";
+cin>>n;
+int k=0;
+m=0;
+for(i=0;i<n;i++)
 {
-    int count = -1, i;
-    for (i = 0; i < t; i++)
-    {
-        if (T[i] == a)
-            return i;
-    }
-    return count;
+cout<<"\nenter the LHS of production";
+cin>>l[i];
+cout<<"\nenter the RHS of production";
+cin>>r;
+for(int j=0;j<2;j++)
+{
+if((r[j]=='(') || r[j]==')' || r[j]=='*' || r[j]=='+' || r[j]=='-' || r[j]=='/' )
+{
+lead[k]=r[j];
+k=k+1;
 }
-void push(char a)
+if((r[j]=='i') && (r[j+1]=='d'))
 {
-    s[top] = a;
-    top++;
+lead[k]=r[j];
+lead[k+1]=r[j+1];
+k=k+1;
 }
-char pop()
-{
-    top--;
-    return s[top];
 }
-void installl(int a, int b)
-
+for(j=1;j<=2;j++)
 {
-    if (l[a][b] == 'f')
-    {
-        l[a][b] = 't';
-        push(T[b]);
-        push(NT[a]);
-    }
+if((r[j]=='(') || r[j]==')' || r[j]=='*' || r[j]=='+' || r[j]=='-' || r[j]=='/' )
+{
+trail[m]=r[j];
+m=m+1;
 }
-void installt(int a, int b)
+if((r[j-1]=='i') && (r[j]=='d'))
 {
-    if (tr[a][b] == 'f')
-    {
-        tr[a][b] = 't';
-        push(T[b]);
-        push(NT[a]);
-    }
+trail[m]=r[j-1];
+trail[m+1]=r[j];
+m=m+1;
+}
 }
 
-int main()
+}
+cout<<"\nthe Leading(A) is :\n";
+cout<<"{  ";
+for(i=0;i<k;i++)
 {
-    cout << "Name : Tushar Kamboj " << endl;
-    cout << "Roll No : 1816110225" << endl;
-    int i, s, k, j, n;
-    char pr[30][30], b, c;
-    cout << "Enter the no of productions:";
-    cin >> n;
-    cout << "Enter the productions one by one\n";
-    for (i = 0; i < n; i++)
-        cin >> pr[i];
-    nt = 0;
-    t = 0;
-    for (i = 0; i < n; i++)
-    {
-        if ((searchnt(pr[i][0])) == -1)
-            NT[nt++] = pr[i][0];
-    }
-    for (i = 0; i < n; i++)
-    {
-        for (j = 3; j < strlen(pr[i]); j++)
-        {
-            if (searchnt(pr[i][j]) == -1)
-            {
-                if (searchter(pr[i][j]) == -1)
-                    T[t++] = pr[i][j];
-            }
-        }
-    }
-    for (i = 0; i < nt; i++)
-    {
-        for (j = 0; j < t; j++)
-            l[i][j] = 'f';
-    }
-    for (i = 0; i < nt; i++)
-    {
-        for (j = 0; j < t; j++)
+if((lead[i]=='i') && (lead[i+1]=='d'))
+cout<<lead[i]<<lead[i+1]<<"  ";
+else
+cout<<lead[i]<<"  ";
+}
+cout<<"}";
+cout<<"\nthe Trailing(A) is :\n";
+cout<<"{  ";
+for(i=0;i<m;i++)
+{
+if((trail[i]=='i') && (trail[i+1]=='d'))
+cout<<trail[i]<<trail[i+1]<<"  ";
+else
+cout<<trail[i]<<"  ";
+}
+cout<<"}";
 
-            tr[i][j] = 'f';
-    }
-    for (i = 0; i < nt; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            if (NT[(searchnt(pr[j][0]))] == NT[i])
-            {
-                if (searchter(pr[j][3]) != -1)
-                    installl(searchnt(pr[j][0]), searchter(pr[j][3]));
-                else
-                {
-                    for (k = 3; k < strlen(pr[j]); k++)
-                    {
-                        if (searchnt(pr[j][k]) == -1)
-                        {
-                            installl(searchnt(pr[j][0]), searchter(pr[j][k]));
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    while (top != 0)
-    {
-        b = pop();
-        c = pop();
-        for (s = 0; s < n; s++)
-        {
-            if (pr[s][3] == b)
-                installl(searchnt(pr[s][0]), searchter(c));
-        }
-    }
-    for (i = 0; i < nt; i++)
-    {
-        cout << "Leading[" << NT[i] << "]"
-             << "\t{";
-        for (j = 0; j < t; j++)
-        {
-            if (l[i][j] == 't')
-                cout << T[j] << ",";
-        }
-        cout << "}\n";
-    }
-
-    top = 0;
-    for (i = 0; i < nt; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            if (NT[searchnt(pr[j][0])] == NT[i])
-            {
-                if (searchter(pr[j][strlen(pr[j]) - 1]) != -1)
-                    installt(searchnt(pr[j][0]), searchter(pr[j][strlen(pr[j]) - 1]));
-                else
-                {
-                    for (k = (strlen(pr[j]) - 1); k >= 3; k--)
-                    {
-                        if (searchnt(pr[j][k]) == -1)
-                        {
-                            installt(searchnt(pr[j][0]), searchter(pr[j][k]));
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    while (top != 0)
-    {
-        b = pop();
-        c = pop();
-        for (s = 0; s < n; s++)
-        {
-            if (pr[s][3] == b)
-                installt(searchnt(pr[s][0]), searchter(c));
-        }
-    }
-    for (i = 0; i < nt; i++)
-    {
-        cout << "Trailing[" << NT[i] << "]"
-             << "\t{";
-        for (j = 0; j < t; j++)
-        {
-            if (tr[i][j] == 't')
-                cout << T[j] << ",";
-        }
-        cout << "}\n";
-    }
-    return 0;
+getch();
 }
